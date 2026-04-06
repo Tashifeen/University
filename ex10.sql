71d30df0aff84bb5c930c692e124b29ca34b20fe
Tashifeen <tashifeen@outlook.com>
2026-04-06 18:57:06 +0100

    First C++ practice

--- ex10.sql
+++
@@ -1,6 +0,0 @@
--- Find each building in the university and the total capacity of all rooms it contains
-	-- 'GROUP BY' operation ensures each room capacity sum is calaculated in diffrent buildings (as multiple rooms can be in 1 building)
-	  
-SELECT building, SUM(capacity) AS total_capacity -- Source for 'SUM' operation: https://www.w3schools.com/sql/sql_sum.asp
-FROM ROOM
-GROUP BY building; -- 'GROUP BY vs ORDER BY': https://www.geeksforgeeks.org/sql/difference-between-order-by-and-group-by-clause-in-sql/
--- ex8.sql
+++
@@ -1,4 +0,0 @@
-# Source: https://blog.pesky.moe/posts/2024-12-01-import-csv-sqlite/
-.mode csv
-.import students.csv students
-.import faculty.csv faculty
